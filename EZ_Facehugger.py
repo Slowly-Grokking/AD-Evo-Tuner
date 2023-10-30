9510fdbee6d6b85dda91cffab65a1020d9b10da3
Slowly-Grokking <61430731+Slowly-Grokking@users.noreply.github.com>
2023-10-30 11:46:48 -0500

    Update EZ_Facehugger.py
    sha256 check for safetensors, bin, and ckpt 
    size check for others
    #these checks will prevent a lot of troubleshooting down the road
    #with new users...
    
    skip files that have already been downloaded
    added download progress

--- EZ_Facehugger.py
+++ EZ_Facehugger.py
@@ -1,5 +1,8 @@
 import requests
 import os
+from tqdm import tqdm
+import hashlib
+import re
 
 # Define the base URL for your Hugging Face repository
 base_download_url = "https://huggingface.co/runwayml/stable-diffusion-v1-5/resolve/main"
@@ -23,38 +26,85 @@ file_urls = [
     f"{base_download_url}/unet/diffusion_pytorch_model.fp16.safetensors",    
 ]
 
-script_directory = os.path.dirname(os.path.abspath(__file__))
-
-
 local_file_paths = [
-    "text_encoder/config.json",   
-    "unet/config.json",   
-    "vae/config.json", 
+    "text_encoder/config.json",
+    "unet/config.json",
+    "vae/config.json",
     "tokenizer/merges.txt",
     "tokenizer/special_tokens_map.json",
     "tokenizer/tokenizer_config.json",
     "tokenizer/vocab.json",
     "scheduler/scheduler_config.json",
     "model_index.json",
-    "safety_checker/pytorch_model.bin", 
+    "safety_checker/pytorch_model.bin",
+    "text_encoder/pytorch_model.bin",
     "text_encoder/model.fp16.safetensors",
+    "vae/diffusion_pytorch_model.bin",
     "vae/diffusion_pytorch_model.fp16.safetensors",
-    "unet/diffusion_pytorch_model.fp16.safetensors",
+    "unet/diffusion_pytorch_model.bin",
+    "unet/diffusion_pytorch_model.fp16.safetensors"
 ]
 
+script_directory = os.path.dirname(os.path.abspath(__file__))
 sd_root = "models/StableDiffusion/"
 
-for file_path in local_file_paths:
-    file_url = f"{base_download_url}/{file_path}"
+def download_file(url, local_path):
+    response = requests.get(url, stream=True)
+    total_size = int(response.headers.get('content-length', 0))
+    with open(local_path, 'wb') as file:
+        with tqdm(total=total_size, unit='B', unit_scale=True, desc=local_path, leave=True) as pbar:
+            for data in response.iter_content(chunk_size=1024):
+                file.write(data)
+                pbar.update(len(data))
 
-    # Create the directory if it doesn't exist
-    local_path = os.path.join(script_directory, sd_root, file_path)
-    os.makedirs(os.path.dirname(local_path), exist_ok=True)
+for url, local_path in zip(file_urls, local_file_paths):
+    # Determine full local path
+    local_path_full = os.path.join(script_directory, sd_root, local_path)
+    os.makedirs(os.path.dirname(local_path_full), exist_ok=True)
 
-    response = requests.get(file_url)
-    if response.status_code == 200:
-        with open(local_path, 'wb') as file:
-            file.write(response.content)
-        print(f"Downloaded: {local_path}")
-    else:
-        print(f"Failed to download: {file_url}")
+    should_download = False
+    # For bin, safetensors, and ckpt files, perform SHA-256 check
+    if local_path_full.endswith(('.bin', '.safetensors', '.ckpt')):
+        try:
+            with open(local_path_full, 'rb') as file:
+                local_sha256 = hashlib.sha256(file.read()).hexdigest()
+        except FileNotFoundError:
+            print(f"Local file {local_path_full} not found. Redownloading.")
+            should_download = True
+        else:
+            sha256_url = url.replace("/resolve/", "/raw/")
+            response = requests.get(sha256_url)
+
+            remote_sha256_response = response.text
+            match = re.search(r'sha256:(\w+)', remote_sha256_response)
+
+            if match:
+                remote_sha256 = match.group(1)
+                if local_sha256 != remote_sha256:
+                    print(f"local_sha256 {local_sha256} doesn't match remote {remote_sha256}. Redownloading: {local_path_full}")
+                    should_download = True
+                else:
+                    print(f"Already have {local_path_full}. Skipping...")
+            else:
+                print(f"Failed to extract SHA-256 hash from response.")
+                should_download = True
+    else:  # For txt and json files, check file size
+        response = requests.head(url)
+        remote_size = int(response.headers.get('content-length', 0))
+        try:
+            local_size = os.path.getsize(local_path_full)
+        except FileNotFoundError:
+            local_size = 0
+
+        if local_size != remote_size:
+            print(f"Local file size {local_size} doesn't match remote size {remote_size}. Redownloading: {local_path_full}")
+            should_download = True
+        else:
+            print(f"Already have{local_path_full}.")
+
+    if should_download:
+        try:
+            download_file(url, local_path_full)
+            print(f"Downloaded: {local_path_full}")
+        except Exception as e:
+            print(f"Failed to download {url}: {e}")
